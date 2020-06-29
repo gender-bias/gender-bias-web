fecc300b494c6230641e636eb0e5505a2f0fbb11
Janet Davis <davisj@whitman.edu>
2020-06-29 15:05:04 -0700

    Created tests for the Header component As part of this, we set up mocha, chai, and puppeteer. We loosely followed the tutorial at https://medium.com/@ankit_m/ui-testing-with-puppeteer-and-mocha-part-1-getting-started-b141b2f9e21

---
+++ test/Header.spec.js
@@ -0,0 +1,30 @@
+describe('The header', function () {
+    let page;
+  
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://localhost:8080');
+    });
+  
+    after (async function () {
+      await page.close();
+    })
+
+    it('should show the name of the app', async function () {
+      const HEADING_SELECTOR = 'h1.title';
+      let heading;
+  
+      await page.waitFor(HEADING_SELECTOR);
+      heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+  
+      expect(heading).to.eql('Reading for Gender Bias');
+    });
+  
+    it('should include a paragraph of description after the app name', async function () {
+      const DESCR_SELECTOR = 'h1.title+p'; 
+  
+      await page.waitFor(DESCR_SELECTOR);
+  
+      expect(await page.$$(DESCR_SELECTOR)).to.have.lengthOf(1);
+    });
+  });
