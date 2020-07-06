3d3a8f5da14879bfd108401f37918f6ffe671d2f
nidhi <nidhi.jaltare3@gmail.com>
2020-07-06 14:31:35 -0700

    blurb tests

--- test/Blurb.spec.js
+++ test/Blurb.spec.js
@@ -17,28 +17,24 @@ describe('A blurb', function() {
         await page.close();
     });
 
-    // #1 (Done)
     it('should appear', async function() {
         await page.waitFor("#container");
-        // We thought that there shouldn't be only one blurb, and since we are uncertain how many blurbs we have, so we don't expect any length! 
-        // We thought of having a variable that will track the number of blurbs, and we will expect the length to equal to that variable, 
-        // but it is not an elegant way of coding. 
     });
-    // #2 
+    
     it('should have the style class "notice"', async function() {
         await page.waitFor(SEL_NOTICE);
         expect(await page.$$(SEL_NOTICE)).to.have.lengthOf(1);
     });
-    // #3 
+
     it('should have the style class "negativeBias"', async function() {
         await page.waitFor('.negativeBias');
     });
-    // #4 
+
     it('should be highlighted on mouseover', async function() {
         await page.hover(SEL_NOTICE);
         expect(await page.$$(SEL_NOTICE)).to.have.lengthOf(1);
     });
-    // #5 
+    
     it('should display a tooltip on mouseover', async function(){
         await page.waitFor(SEL_TOOLTIP);
 
@@ -48,14 +44,13 @@ describe('A blurb', function() {
 
     describe('The tooltip', function() {
 
-        // #6
         it('should have a title', async function(){
             await page.waitFor(SEL_TOOLTIP);
 
             expect(await page.$(SEL_TOOLTIP)).to.not.be.undefined;
 
         });
-        // #7
+
         it('should have some further text', async function(){
             await page.waitFor(SEL_TOOLTIP);
 
@@ -65,10 +60,8 @@ describe('A blurb', function() {
     });
 
     describe('Other text', async function() {
-
-        // #8
-        it('should not have the style class "notice"', async function() {
-
+        let SEL_UNFLAGGED;
+        before(async function() {
             const SEL_UNFLAGGED = await page.evaluate(() => {
                 const string = 'Some';
                 const selector = '#feedback #container';
@@ -80,12 +73,22 @@ describe('A blurb', function() {
                     }
                 }
             });
+        });
+
+        it('should not have the style class "notice"', async function() {
+
+            const element = await page.$(SEL_UNFLAGGED + " .notice");
+            expect(element).to.be.null;
+
+        });
+        
+        it('should not be highlighted or display a tooltip on mouseover', async function(){
+            await page.hover(SEL_NOTICE);
 
             const element = await page.$(SEL_UNFLAGGED + " #tooltip");
             expect(element).to.be.null;
 
         });
-        // #9
-        it('should not be highlighted or display a tooltip on mouseover');
+
     });
 });
