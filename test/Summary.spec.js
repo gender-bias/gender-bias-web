030ef7b5677c766a0d9f2d694ccf5b89d0739877
[dylanjwu] <dylanjwu@gmail.com>
2020-07-16 14:51:45 -0400

    Fix 2nd to last test, all tests but last pass now

--- test/Summary.spec.js
+++ test/Summary.spec.js
@@ -9,7 +9,6 @@ describe('The Summary', function() {
         await page.close();
     });
 
-
     describe('Initially', function() {
 
         it("it shouldn't exist until the Sidebar component is shown", async function() {
@@ -30,26 +29,23 @@ describe('The Summary', function() {
             await button.click();
         });
 
-
         it('it should appear after submission', async function() {
             const summariescontainer = await page.$(SEL_SIDEBAR_CONTAINER);
             expect(summariescontainer).to.not.be.null;
 
         });
 
-        //This test passes some times and fails other times
-        //When it fails, element has length 0
         it('it should have the a real summary text', async function() {
-            const element = await page.$$(SEL_ISSUE_P);
-            expect(element).to.have.lengthOf(2);
+            await page.waitFor(SEL_ISSUE_P);
+            expect(await page.$$(SEL_ISSUE_P)).to.have.lengthOf(2);
         });
 
-        it('it should be highlighted on mouseover', async function() {
-            // const element = await page.$('.issue');
-            // console.log(element);
-            // await page.hover("issue");
-            // let color = await page.evaluate(element => element.style.color, element);
-            // expect(element.style.color).to.equal('');
+        it('it should be highlighted on mouseover', function() {
+            this.skip('not sure how to test style on a hover event.');
+
+            // const element = await page.hover('.issue');
+            // const color = await page.evaluate(element => element, element);
+            // expect(color).to.be.null;
         });
 
     });
