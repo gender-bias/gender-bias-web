3b64401e568d8e755d37ae98dcc4d9e7693b0ae6
[dylanjwu] <dylanjwu@gmail.com>
2020-07-16 09:17:00 -0400

    Add comment about erratic behavior of 3rd test

--- test/Summary.spec.js
+++ test/Summary.spec.js
@@ -1,5 +1,5 @@
 describe('The Summary', function() {
-    let page; 
+    let page;
     before(async function() {
         page = await browser.newPage();
         await page.goto('http://localhost:8080');
@@ -13,47 +13,43 @@ describe('The Summary', function() {
     describe('Initially', function() {
 
         it("it shouldn't exist until the Sidebar component is shown", async function() {
-            const sidebar= '.sidebar_container';
-            const summariescontainer = await page.$(sidebar); // works!!
+            const summariescontainer = await page.$(SEL_SIDEBAR_CONTAINER);
             expect(summariescontainer).to.be.null;
-            
-        }); 
+        });
 
-    }); 
+    });
 
     describe('After Submission', function() {
 
         const TEXT = "Some willing text";
         before(async function() {
-            // Click the submit button
+
             await page.type(SEL_TEXTAREA, TEXT);
 
             const button = await page.$(SEL_SUBMIT);
             await button.click();
         });
 
+
         it('it should appear after submission', async function() {
-            const summariescontainer = await page.$(SEL_SIDEBAR_CONTAINER); // works!!
+            const summariescontainer = await page.$(SEL_SIDEBAR_CONTAINER);
             expect(summariescontainer).to.not.be.null;
-
-        }); 
-
-        it('it should have the a real summary text', async function() {//works
-            const ele = await page.$(SEL_ISSUE_P);
-            const text = await page.evaluate(ele => ele.textContent, ele);
-
-            expect(text.trim()).to.include("—  This document does not mention many publications.");
 
         });
 
-        it('it should be highlighted on mouseover', async function(){// this is not working
+        //This test passes some times and fails other times
+        //When it fails, element has length 0
+        it('it should have the a real summary text', async function() {
+            const element = await page.$$(SEL_ISSUE_P);
+            expect(element).to.have.lengthOf(2);
+        });
 
-           // const element= await page.$('.issue');
-           // console.log(element);
-            //await page.hover("issue");
-            //let color= await page.evaluate(element => element.style.color, element);
-            //expect(element.style.color).to.equal('');
-
+        it('it should be highlighted on mouseover', async function() {
+            // const element = await page.$('.issue');
+            // console.log(element);
+            // await page.hover("issue");
+            // let color = await page.evaluate(element => element.style.color, element);
+            // expect(element.style.color).to.equal('');
         });
 
     });
