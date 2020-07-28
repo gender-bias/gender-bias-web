bbf790566fbed34639e34a2474011b6494e16bf2
[dylanjwu] <dylanjwu@gmail.com>
2020-07-28 11:16:39 -0400

    remove unused sels, list by component in bootstrap

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -10,21 +10,25 @@ const globalVariables = _.pick(global, ['browser', 'expect']);
 before(async function() {
     global.expect = expect;
     global.browser = await puppeteer.launch();
+
+    // Main
     global.SEL_TEXTAREA = 'textarea';
     global.SEL_SUBMIT = '.submit-button';
     global.SEL_BACK = '.back-button';
     global.SEL_BLURBS = '.blurbs';
-    global.SEL_SIDEBAR = '.sidebar_container';
-    global.SEL_READOUT = '.readout_container';
-    global.SEL_SUMMARY = '#summary';
+
+    // Blurb
     global.SEL_CONTAINER = '#container';
     global.SEL_NEGATIVEBIAS = '.negativeBias';
     global.SEL_TOOLTIP = 'span#tooltip h1';
     global.SEL_NOTICE = '.notice';
+
+    // Summary
     global.SEL_SIDEBAR_CONTAINER = '.sidebarContainer';
     global.SEL_ISSUE = '.issue';
     global.SEL_ISSUE_P = '.issue p';
     global.SEL_ISSUE_HOVER = ".issue:hover";
+
     global.TEXT = "Some willing text";
     global.PAGE_URL = 'http://localhost:8080/serve-vue-app/';
 });
